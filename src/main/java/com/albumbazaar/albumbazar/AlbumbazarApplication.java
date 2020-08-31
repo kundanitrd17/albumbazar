56dfbeb6eb3ec994a1ef3b6596368cb9404d8490
kundanitrd17 <kundanitrd17@gmail.com>
2020-08-31 13:09:25 +0530

    initialize repo

---
+++ src/main/java/com/albumbazaar/albumbazar/AlbumbazarApplication.java
@@ -0,0 +1,13 @@
+package com.albumbazaar.albumbazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumbazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumbazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albumbazaar/albumbazar/AlbumbazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albumbazaar.albumbazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumbazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
