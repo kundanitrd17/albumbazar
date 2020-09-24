c274e959efe5626cda8fc90543b1f688d1778eec
princewillzz <princewillz2013@gmail.com>
2020-09-24 15:11:02 +0530

    Built the storage service to store image into a particular folder.

--- src/main/java/com/albumbazaar/albumbazar/AlbumbazarApplication.java
+++ src/main/java/com/albumbazaar/albumbazar/AlbumbazarApplication.java
@@ -1,14 +1,29 @@
 package com.albumbazaar.albumbazar;
 
+import com.albumbazaar.albumbazar.services.storage.ImageStorageProperties;
+import com.albumbazaar.albumbazar.services.storage.StorageService;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ImageStorageProperties.class)
 public class AlbumbazarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbumbazarApplication.class, args);
 		System.out.println("Application started");
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
---
+++ src/main/java/com/albumbazaar/albumbazar/controller/FileUploadController.java
@@ -0,0 +1,50 @@
+package com.albumbazaar.albumbazar.controller;
+
+import com.albumbazaar.albumbazar.services.storage.StorageFileNotFoundException;
+import com.albumbazaar.albumbazar.services.storage.StorageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(@Qualifier("imageStorageService") StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/upload")
+    public String uploader() {
+        return "upload";
+    }
+
+    @PostMapping(value = "/upload")
+    @ResponseBody
+    public String upload(@RequestParam("files") MultipartFile files) {
+
+        try {
+            storageService.store(files);
+        } catch (Exception e) {
+            return "Exception";
+        }
+        return "Done";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFoundException(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+}
